599958d8176697357045a91108d09f9059c6b740
remeezsanik <remeez@ispgweb.com>
2023-07-04 12:02:12 +0530

    header and search component

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const SearchBox = () => {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input) return;
+    navigate(`/search?term=${input}`);
+  };
+
+  return (
+    <div>
+      <form
+        className="max-w-6xl mx-auto flex justify-between items-center px-5"
+        onSubmit={handleSearch}
+      >
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Search here..."
+          className="flex-1 p-2 w-full h-14 rounded-sm placeholder-gray-500 text-gray-500 outline-none bg-transparent dark:text-orange-400"
+        />
+        <button
+          className="text-orange-400 disabled:text-gray-400"
+          type="submit"
+          disabled={!input}
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBox;
---
+++ src/constants/categories.ts
@@ -0,0 +1,6 @@
+
+
+type categories = "general"|"business"| "entertainment"| "health"|"science"|"sports"| "technology"
+
+
+export const categories: categories[]= ["general","business", "entertainment", "health","science","sports", "technology"]
